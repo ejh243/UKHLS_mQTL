7a9689e791c0c21a170f5fdfaef73975d76d358a
ejh243 <e.j.hannon@ex.ac.uk>
2018-02-02 09:36:39 +0000

    Update collateSMROutputMqtlEqtl.r

--- collateSMROutputMqtlEqtl.r
+++ collateSMROutputMqtlEqtl.r
@@ -18,6 +18,12 @@ charProbeDist<-function(term, cpgAnno){
 	}
 }
 
+## function to separate genic annotation category for DNAm sites
+sepByFeature<-function(term, signifPairs){
+	return(signifPairs$b_SMR[grep(term, signifPairs$UCSC_RefGene_Group)])
+
+}
+
 epicManifest<-read.csv("MethylationEPIC_v-1-0_B2.csv", skip = 7)
 
 setwd("")
@@ -45,14 +51,13 @@ out<-cbind(out, epicManifest[match(out$each, epicManifest$IlmnID),
 
 save(out, file = "AllmQTL_eQTL_ColocResults.rda")
 
+## Summarise number of times each feature tested
 table(table(out$each))
 table(table(out$ProbeID))
 par(mfrow = c(1,3))
 hist(table(out$each), xlab = "Number of times DNAm site tested")
 hist(table(out$ProbeID), xlab = "Number of times transcript tested")
 hist(table(out$Gene), xlab = "Number of times gene tested")
-
-write.csv(out, "../AllTested_mQTL_eQTL_pairs.csv")
 
 ## how many genes each DNAm site tested against?
 summary(as.numeric(table(unlist(lapply(strsplit((unique(paste(out$each, out$Gene))), " "), head, n = 1)))))
@@ -62,23 +67,20 @@ sd(as.numeric(table(out$each)))
 summary(as.numeric(table(out$ProbeID)))
 sd(as.numeric(table(out$ProbeID)))
 
-write.csv(out[which(as.numeric(out$p_SMR) < 1.02e-07),], "../BonfSignif_mQTL_eQTL_pairs.csv")
-## Supp Table 7
-write.csv(out[which(as.numeric(out$p_SMR) < 1.02e-07 & as.numeric(out$p_HET) > 0.05),],"../BonfSignif_HetP_mQTL_eQTL_pairs.csv")
-
 length(unique(out$Gene[which(as.numeric(out$p_SMR) < 1.02e-07)]))/length(unique(out$Gene))
 length(unique(out$ProbeID[which(as.numeric(out$p_SMR) < 1.02e-07)]))/length(unique(out$ProbeID))
 length(unique(out$each[which(as.numeric(out$p_SMR) < 1.02e-07)]))/length(unique(out$each))
 
-
 signifPairs<-out[which(as.numeric(out$p_SMR) < 1.02e-07 & as.numeric(out$p_HET) > 0.05),]
+write.csv(signifPairs,"SupplementaryTable7.csv")
 
+## Summarise number of times each feature was significantly associated
 table(table(signifPairs$each))
 table(table(signifPairs$ProbeID))
 par(mfrow = c(1,3))
-hist(table(signifPairs$each), xlab = "Number of times DNAm site tested")
-hist(table(signifPairs$ProbeID), xlab = "Number of times transcript tested")
-hist(table(signifPairs$Gene), xlab = "Number of times gene tested")
+hist(table(signifPairs$each), xlab = "Number of times DNAm site associated")
+hist(table(signifPairs$ProbeID), xlab = "Number of times transcript associated")
+hist(table(signifPairs$Gene), xlab = "Number of times gene associated")
 
 summary(as.numeric(table(unlist(lapply(strsplit((unique(paste(signifPairs$each, signifPairs$Gene))), " "), head, n = 1)))))
 sd(as.numeric(table(unlist(lapply(strsplit((unique(paste(signifPairs$each, signifPairs$Gene))), " "), head, n = 1)))))
@@ -93,6 +95,7 @@ length(unique(signifPairs$each))/length(unique(out$each))
 
 geneFeatures<-unique(unlist(strsplit(as.character(out$UCSC_RefGene_Group), ";")))
 
+## Enrichment testing
 backgroundAnno<-unique(out[,c("each", "CHR", "MAPINFO", "UCSC_RefGene_Name", "UCSC_RefGene_Group", 
   "Relation_to_UCSC_CpG_Island", "Regulatory_Feature_Name", "Regulatory_Feature_Group", "DNase_Hypersensitivity_NAME", 
   "OpenChromatin_NAME", "TFBS_Evidence_Count", "Methyl450_Loci", "SNP_ID", "SNP_DISTANCE", "SNP_MinorAlleleFrequency")])
@@ -116,20 +119,15 @@ tab["Intergenic",2]<-length(which(backgroundAnno$UCSC_RefGene_Group == ""))
 tab[,3]<-tab[,1]/length(unique(signifAnno$each))*100
 tab[,4]<-tab[,2]/length(unique(backgroundAnno$each))*100
 
+## Supp Figure 15
 par(mfrow = c(1,1))
 barplot(t(tab[,3:4]), beside = TRUE, col = c("blue", "lightgray"), ylab = "% annotated to gene feature")
 legend("topright", c("Significant DNAm sites", "Background"), pch = 15, col = c("blue", "lightgray"))
 
-vioData<-NULL
 for(each in geneFeatures){
 	tab[each, 1]<-length(grep(each, signifAnno$UCSC_RefGene_Group))
 	tab[each, 2]<-length(grep(each, backgroundAnno$UCSC_RefGene_Group))
 	
-}
-
-sepByFeature<-function(term, signifPairs){
-	return(signifPairs$b_SMR[grep(term, signifPairs$UCSC_RefGene_Group)])
-
 }
 
 vioData<-sapply(geneFeatures, sepByFeature, signifPairs)
@@ -137,7 +135,9 @@ vioData[[8]]<-signifPairs$b_SMR[which(signifPairs$UCSC_RefGene_Group == "")]
 vioplot(vioData[[1]], vioData[[2]], vioData[[3]], vioData[[4]], vioData[[5]], vioData[[6]], vioData[[7]], vioData[[8]], names = c(geneFeatures, "Intergenic"))
 
 vioData.abs<-lapply(vioData, abs)
-vioplot(vioData.abs[[1]], vioData.abs[[2]], vioData.abs[[3]], vioData.abs[[4]], vioData.abs[[5]], vioData.abs[[6]], vioData.abs[[7]], vioData.abs[[8]], names = c(geneFeatures, "Intergenic"))
+## Supp Fig 16
+vioplot(vioData.abs[[1]], vioData.abs[[2]], vioData.abs[[3]], vioData.abs[[4]], vioData.abs[[5]], vioData.abs[[6]], 
+	vioData.abs[[7]], vioData.abs[[8]], names = c(geneFeatures, "Intergenic"))
 
 annoMatch<-rep(NA, nrow(signifPairs))
 for(i in 1:nrow(signifPairs)){
@@ -147,6 +147,7 @@ for(i in 1:nrow(signifPairs)){
 		annoMatch[i]<-0
 	}
 }
+
 
 ## how many are not annotated to the same gene but are tested against
 annoMatch.all<-rep(NA, nrow(out))
@@ -170,7 +171,6 @@ table(unique(sig.same$each) %in% unique(sig.opp$each))
 table(unique(sig.opp$each) %in% unique(sig.same$each))
 
 ## for cpgs associated with a diff gene, were they tested against themselves?
-
 table(unique(sig.opp$each) %in% unique(out$each[which(annoMatch.all == 1)]))
 table(unique(sig.opp$each) %in% unique(sig.same$each))
 table(unique(sig.opp$each) %in% unique(out$each[which(annoMatch.all == 1)]))
